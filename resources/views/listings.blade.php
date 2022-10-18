00be1b2af36a3ca0c339dafc8ea7de7af6f6a38f
sunshade88 <jasonsglim@gmail.com>
2022-10-18 15:06:02 +0800

    initial change to main

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,7 @@
+<h1>{{ $heading }}</h1>
+<h2>
+  @foreach ($listings as $listing)
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+  @endforeach
+</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +19,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return response('<h1>hello world</h1>');
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolores provident necessitatibus animi culpa. In, cupiditate nam! Hic reprehenderit dolore, accusantium veritatis soluta est culpa',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'alias similique quasi minima quas quia id nisi nesciunt maiores, nemo, laboriosam ad. Quam eligendi beatae aliquid dolor. Quos corrupti impedit quibusdam alias aliquam ipsum?',
+            ],
+        ],
+    ]);
 });
 
-Route::get('/posts/{id}', function ($id) {
-    //dd($id);
-    return response('Post ' . $id);    
-})->where('id', '[0-9]+');
+//
